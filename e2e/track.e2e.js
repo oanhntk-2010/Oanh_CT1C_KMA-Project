b2c8340e49203a128bcae71b5fad2475ac87f96d
oanhntk-2010 <85410885+oanhntk-2010@users.noreply.github.com>
2021-07-14 00:30:52 +0700

    Update track.e2e.js

--- e2e/track.e2e.js
+++ e2e/track.e2e.js
@@ -9,13 +9,13 @@ describe('Track Screen Testing', () => {
     // await element(by.id('trackTab')).tap();
   });
 
-  // TC03
+ 
   it('should show track screen after tap', async () => {
     await expect(element(by.id('trackScreen'))).toBeVisible();
   });
 
   // Check rename track //
-  // TC52, TC53, TC66, TC67
+  // TC01, TC03, TC09, TC10
   it('should show rename options', async () => {
     await expect(element(by.id('trackOption0'))).toBeVisible();
     await element(by.id('trackOption0')).tap();
@@ -25,11 +25,11 @@ describe('Track Screen Testing', () => {
     await expect(element(by.text('CANCEL'))).toBeVisible();
     await expect(element(by.text('RENAME'))).toBeVisible();
 
-    // TC66
+    // TC09
     await element(by.text('CANCEL')).tap();
     await expect(element(by.text('RENAME'))).not.toBeVisible();
 
-    // TC67
+    // TC10
     await element(by.text('Rename')).tap();
     await element(by.text('CANCEL')).multiTap(2);
     await expect(element(by.text('RENAME'))).not.toBeVisible();
@@ -37,7 +37,7 @@ describe('Track Screen Testing', () => {
     await device.pressBack();
   });
 
-  // TC664, TC65
+  // TC08, 
   it('should able to rename song', async () => {
     const songElement = element(
       by.label('Track Option').withAncestor(by.label('Laylalay')),
@@ -70,7 +70,7 @@ describe('Track Screen Testing', () => {
       .toBeVisible()
       .withTimeout(5000);
 
-    // should able to rename alphabet song
+    // TC 04 should able to rename alphabet song
     const azElement = element(
       by.label('Track Option').withAncestor(by.label('Laylalay')),
     );
@@ -86,7 +86,7 @@ describe('Track Screen Testing', () => {
       .toBeVisible()
       .withTimeout(5000);
 
-    // TC55 should able to rename number name song
+    // TC05 should able to rename number name song
     const numElement = element(by.label('Track Option').withAncestor(by.label('abcxyz')));
     await expect(numElement).toBeVisible();
     await numElement.tap();
@@ -100,7 +100,7 @@ describe('Track Screen Testing', () => {
       .toBeVisible()
       .withTimeout(5000);
 
-    // TC58 should able to rename lowercase name
+    // TC06 should able to rename lowercase name
     const lowerElement = element(
       by.label('Track Option').withAncestor(by.label('123456')),
     );
@@ -116,7 +116,7 @@ describe('Track Screen Testing', () => {
       .toBeVisible()
       .withTimeout(5000);
 
-    // TC62 should able to rename uppercase name
+    // TC07 should able to rename uppercase name
     const upperElement = element(
       by.label('Track Option').withAncestor(by.label('bai hat')),
     );
@@ -134,7 +134,7 @@ describe('Track Screen Testing', () => {
   });
 
   // Check delete song //
-  // TC 25, 26, 27, 28, 29
+  // TC12, 13, 14, 15, 16
   it('should able to delete song ', async () => {
     await device.reloadReactNative();
     const songElement = element(
